bea5240338ee7a06b5d01b1a6eccb7db340c5837
Allen Byerly <allenbyerly@gmail.com>
2014-08-08 22:14:39 -0400

    Added description validation

--- spec/features/todo_lists/create_spec.rb
+++ spec/features/todo_lists/create_spec.rb
@@ -48,5 +48,41 @@ describe "Creating To Do Lists" do
 		expect(page).to_not have_content("This is my first example")
 
 	end
+	it "displays an error when the todolist has no description" do
+		expect(TodoList.count).to eq(0)
+
+		visit "/todo_lists"	
+		click_link "New Todo list"
+		expect(page).to have_content("New todo_list")
+
+		fill_in "Title", with: "My Test List 2"
+		fill_in "Description", with: ""
+		click_button "Create Todo list"
+
+		expect(page).to have_content("error")
+		expect(TodoList.count).to eq(0)
+
+		visit "/todo_lists"
+		expect(page).to_not have_content("This is my first example")
+
+	end
+	it "displays an error when the todolist has description less than 3" do
+		expect(TodoList.count).to eq(0)
+
+		visit "/todo_lists"	
+		click_link "New Todo list"
+		expect(page).to have_content("New todo_list")
+
+		fill_in "Title", with: "My Test List 2"
+		fill_in "Description", with: "food"
+		click_button "Create Todo list"
+
+		expect(page).to have_content("error")
+		expect(TodoList.count).to eq(0)
+
+		visit "/todo_lists"
+		expect(page).to_not have_content("This is my first example")
+
+	end
 end
 
